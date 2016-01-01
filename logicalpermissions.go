5b183bf1c555a5e2ca04256f9c4614af332b61b9
Kristofer Tengstrom <info@kristofer-tengstrom.se>
2016-01-01 14:01:12 +0100

    kristofer: Put interface in separate file

--- logicalpermissions.go
+++ logicalpermissions.go
@@ -1,18 +1,9 @@
 package logicalpermissions
 
-type LogicalPermissionsInterface interface {
-  AddType(name string, callback func(string, map[string]interface{}) bool)
-  RemoveType(name string)
-  TypeExists(name string) bool
-  GetTypeCallback(name string) func(string, map[string]interface{}) bool
-  GetTypes() map[string]interface{}
-  SetTypes(types map[string]interface{})
-  GetBypassCallback() func(map[string]interface{}) bool
-  SetBypassCallback(callback func(map[string]interface{}) bool)
-  CheckAccess(permissions map[string]interface{}, context map[string]interface{}) bool
+type LogicalPermissions struct {
+  types map[string]interface{}
+  bypass_callback func(map[string]interface{}) bool
 }
-
-type LogicalPermissions struct {}
 
 func (this *LogicalPermissions) AddType(name string, callback func(string, map[string]interface{}) bool) {
   

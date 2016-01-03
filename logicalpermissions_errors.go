d39cb3674914171b73f70b2cb9fcfa97d43a4d19
Kristofer Tengstrom <info@kristofer-tengstrom.se>
2016-01-03 17:30:51 +0100

    kristofer: Changed indention for errors

--- logicalpermissions_errors.go
+++ logicalpermissions_errors.go
@@ -1,25 +1,25 @@
 package logicalpermissions
 
 type InvalidArgumentValueError struct {
-    msg string
+  msg string
 }
 
 func (this *InvalidArgumentValueError) Error() string { return this.msg }
 
 type InvalidValueForLogicGateError struct {
-    msg string
+  msg string
 }
 
 func (this *InvalidValueForLogicGateError) Error() string { return this.msg }
 
 type PermissionTypeNotRegisteredError struct {
-    msg string
+  msg string
 }
 
 func (this *PermissionTypeNotRegisteredError) Error() string { return this.msg }
 
 type InvalidCallbackReturnTypeError struct {
-    msg string
+  msg string
 }
 
 func (this *InvalidCallbackReturnTypeError) Error() string { return this.msg }

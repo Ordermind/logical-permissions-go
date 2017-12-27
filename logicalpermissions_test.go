80d1c95275d3b8306628c4746f2d5d8e671489ce
Kristofer Tengström <kristofer@linux-s9vx.home>
2017-12-27 15:10:54 +0100

    kristofer: Working on fixing test errors in go tip version

--- logicalpermissions_test.go
+++ logicalpermissions_test.go
@@ -176,8 +176,8 @@ func TestGetTypeCallback(t *testing.T) {
 	if err2 != nil {
 		t.Error(fmt.Sprintf("LogicalPermissions::GetTypeCallback() returned an error: %s", err2))
 	}
-	callback1_sig = fmt.Sprintf("%v", callback1)
-  callback2_sig = fmt.Sprintf("%v", callback2)
+	callback1_sig := fmt.Sprintf("%v", callback1)
+  callback2_sig := fmt.Sprintf("%v", callback2)
 	assert.Equal(t, callback1_sig, callback2_sig)
 }
 
